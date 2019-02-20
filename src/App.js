acdbde4f758a90c3e773f96a509966d54af2c1c8
Joseph Hoppe <jhoppe@srs-health.com>
2019-02-20 07:35:07 -0500

    Added an about component in a separate file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import AboutMe from './aboutme'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Index = () => <h2>Home</h2>;
+const Disclaimer = () => <h2>Disclaimer</h2>;
 
-export default App;
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/aboutme/">About Me</Link>
+          </li>
+          <li>
+            <Link to="/disclaimer/">Disclaimer</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/aboutme/" component={AboutMe} />
+      <Route path="/disclaimer/" component={Disclaimer} />
+    </div>
+  </Router>
+);
+
+export default AppRouter;
---
+++ src/aboutme.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// depending on the full pathname
+const AboutMe = () => (
+    <div>
+        Got here
+    </div>
+)
+
+
+export default AboutMe
