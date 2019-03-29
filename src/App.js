c141c0f0d0860b7bb7bb024beea6b67ce41b30e9
Joseph Hoppe <jhoppe@gmail.com>
2019-03-29 13:20:21 -0400

    Added top nav component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AboutMe from './AboutMe';
 import CardList from './CardList/CardList';
+import TopNav from './TopNav/TopNav.js';
 import 'bootstrap/dist/css/bootstrap.css'
 
 const Index = () => <h2>Some stuff I&lsquo;ve been working on</h2>;
@@ -10,27 +11,7 @@ const Disclaimer = () => <h2>Disclaimer</h2>;
 const AppRouter = () => (
   <Router>
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link to="/" className="navbar-brand">Joseph Hoppe</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/" title="Home">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/AboutMe/" title="About Me">About Me</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/disclaimer/" title="Disclaimer">Disclaimer</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <TopNav />
 
       <div className="container">
         <Route path="/" exact component={Index} />
---
+++ src/TopNav/TopNav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const TopNav = (props) => (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Joseph Hoppe</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon" />
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/" title="Home">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/AboutMe/" title="About Me">About Me</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/disclaimer/" title="Disclaimer">Disclaimer</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+)
+
+export default TopNav
