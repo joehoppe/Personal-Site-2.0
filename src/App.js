fc62fb87473741e92c69a0581ed4449fb1b49ab3
Joseph Hoppe <jhoppe@gmail.com>
2019-03-29 13:13:06 -0400

    Formatted app.js

--- src/App.js
+++ src/App.js
@@ -11,32 +11,33 @@ const AppRouter = () => (
   <Router>
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand">Joseph Hoppe</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" 
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/AboutMe/">About Me</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/disclaimer/">Disclaimer</Link>
-          </li>
-        </ul>
+        <Link to="/" className="navbar-brand">Joseph Hoppe</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/" title="Home">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/AboutMe/" title="About Me">About Me</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/disclaimer/" title="Disclaimer">Disclaimer</Link>
+            </li>
+          </ul>
         </div>
       </nav>
+
       <div className="container">
         <Route path="/" exact component={Index} />
         <Route path="/aboutme/" component={AboutMe} />
         <Route path="/disclaimer/" component={Disclaimer} />
-
       </div>
-    
+
       <CardList top={100} />
     </div>
   </Router>
