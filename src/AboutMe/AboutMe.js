398f894ed3e93caa44b6bdcc6d11e2435ee904c3
Joseph Hoppe <jhoppe@srs-health.com>
2019-05-17 16:20:15 -0400

    Updating LinkedIn link

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -9,23 +9,21 @@ const AboutMe = () => (
 
         <div className="row">
             <div className="row-sm">
+                Find me on <a href="http://www.linkedin.com/in/josephhoppe"target="_blank" rel="noopener noreferrer">LinkedIn</a>.
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="row-sm">
                 <div className="col-med">
-                    <h3>
+                    <div>
                         StackOverflow Profile:
-                    </h3>
+                    </div>
                     <a href="http://stackoverflow.com/users/846844/hoppe" target="_blank" title="Stack Overflow profile" rel="noopener noreferrer">
                         <img src="http://stackoverflow.com/users/flair/846844.png" width="208" height="58" alt="profile for Hoppe at Stack Overflow, Q&amp;A for
                             professional and enthusiast programmers" title="profile for Hoppe at Stack Overflow, Q&amp;A for professional and enthusiast programmers" />
                     </a>
                 </div>
-            </div>
-        </div>
-        <div className="row">
-            <div className="row-sm">
-                <div className="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US"
-                                data-type="horizontal" data-theme="light" data-vanity="josephhoppe">
-                                <a className="LI-simple-link" title="My LinkedIn Profile" style={{ float: "right" }}
-                                    href='https://www.linkedin.com/in/josephhoppe?trk=profile-badge'>Joseph Hoppe</a></div>
             </div>
         </div>
     </section>
--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import TopNav from './TopNav/TopNav';
 import Footer from './Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.css'
 
-const Index = () => <div><h2>Some stuff I&lsquo;ve been working on</h2><CardList top={100} /></div>;
-const DisclaimerComponent = () => <div><h2>Disclaimer</h2><Disclaimer /></div>;
-const AboutMeComponent = () => <h2>About Me<AboutMe /></h2>;
-const NotFoundComponent = () => <div><h2>Not Found</h2><NotFound /></div>;
+const Index = () => <React.Fragment><h2>Some stuff I&lsquo;ve been working on</h2><CardList top={100} /></React.Fragment>;
+const DisclaimerComponent = () => <React.Fragment><h2>Disclaimer</h2><Disclaimer /></React.Fragment>;
+const AboutMeComponent = () => <React.Fragment><h2>About Me</h2><AboutMe /></React.Fragment>;
+const NotFoundComponent = () => <React.Fragment><h2>Not Found</h2><NotFound /></React.Fragment>;
 
 const AppRouter = () => (
   <Router>
